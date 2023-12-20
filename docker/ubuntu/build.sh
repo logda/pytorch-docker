b022f8dc2f18386970aa2e0860df04beca99da12
yida <caiyida19@gmail.com>
2023-12-20 17:15:05 +0800

    registry

--- docker/ubuntu/build.sh
+++ docker/ubuntu/build.sh
@@ -8,6 +8,6 @@ docker build \
     --build-arg TORCHAUDIO_VERSION=${TORCHAUDIO_VERSION} \
     --build-arg TORCHAUDIO_VERSION_SUFFIX=${TORCHAUDIO_VERSION_SUFFIX} \
     --build-arg PYTORCH_DOWNLOAD_URL=${PYTORCH_DOWNLOAD_URL} \
-    -t logda/pytorch:${IMAGE_TAG} \
+    -t ${DOCKERHUB_URL}:${IMAGE_TAG} \
     -f docker/ubuntu/Dockerfile \
     .
